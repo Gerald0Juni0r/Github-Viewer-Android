057da955cd3341dec7c65845d2624dc36e004657
Gerald0Juni0r <geraldo.gj63@gmail.com>
2025-02-10 16:28:29 -0300

    Atualizando GitHubAPI.java

--- app/src/main/java/com/example/github_viewer/api/GitHubAPI.java
+++ app/src/main/java/com/example/github_viewer/api/GitHubAPI.java
@@ -3,6 +3,9 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface GitHubAPI { // Interface GitHubAPI
-    @GET("/search/users")
-    Call<UserResponse> searchUsers(@Query("q") String query); // Método searchUsers
+    @GET("/users/{username}")
+    Call<User> getUser(@Path("username") String username); // Método searchUsers
+
+    @GET("/users/{username}/repos")
+    Call<List<Repository>> getUserRepositories(@Path("username") String username); // Método getUserRepositories
 }
