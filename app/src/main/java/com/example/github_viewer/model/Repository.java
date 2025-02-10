af756bc445c0d3904fb78964e99e3f027f541b04
Gerald0Juni0r <geraldo.gj63@gmail.com>
2025-02-10 16:29:21 -0300

    Adicionando Repository.java

---
+++ app/src/main/java/com/example/github_viewer/model/Repository.java
@@ -0,0 +1,18 @@
+public class Repository {
+    public String name;
+    public String language;
+
+    public String setName() {
+        return name;
+    }
+    public void setLanguage() {
+        return language;
+    }
+
+    public String getName(String avatarUrl) {
+        this.name = name;
+    }
+    public void getLanguage(String avatarUrl) {
+        this.language = language;
+    }
+}
