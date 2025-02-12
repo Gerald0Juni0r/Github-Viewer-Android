85cafc751cd383f5b10e0f5efa2c435ea742642b
Gerald0Juni0r <geraldo.gj63@gmail.com>
2025-02-12 19:15:53 -0300

    Atualizando GitHubAPI.java

--- app/src/main/java/com/example/github_viewer/api/GitHubAPI.java
+++ app/src/main/java/com/example/github_viewer/api/GitHubAPI.java
@@ -1,11 +1,25 @@
+package com.example.github_viewer.api;
+
+import com.example.github_viewer.model.Repository;
+import com.example.github_viewer.model.User;
+
+import java.util.List;
+import java.util.Map;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Query;
+import retrofit2.http.Path;
 
-public interface GitHubAPI { // Interface GitHubAPI
-    @GET("/users/{username}")
-    Call<User> getUser(@Path("username") String username); // Método searchUsers
+public interface GitHubAPI {
+    @GET("users/{username}")
+    Call<User> getUserProfile(@Path("username") String username);
 
-    @GET("/users/{username}/repos")
-    Call<List<Repository>> getUserRepositories(@Path("username") String username); // Método getUserRepositories
+    @GET("users/{username}/repos")
+    Call<List<Repository>> getUserRepositories(@Path("username") String username);
+
+    @GET("repos/{owner}/{repo}/languages")
+    Call<Map<String, Integer>> getRepositoryLanguages(
+            @Path("owner") String owner,
+            @Path("repo") String repo
+    );
 }
