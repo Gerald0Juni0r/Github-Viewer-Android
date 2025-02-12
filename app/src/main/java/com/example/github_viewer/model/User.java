61de7f4a8cbbccb684fc736057bf63f759edf370
Gerald0Juni0r <geraldo.gj63@gmail.com>
2025-02-12 19:16:50 -0300

    Atualizando User.java

--- app/src/main/java/com/example/github_viewer/model/User.java
+++ app/src/main/java/com/example/github_viewer/model/User.java
@@ -1,26 +1,49 @@
+package com.example.github_viewer.model;
+
 public class User {
     private String login;
-    private String avatarUrl;
-    private String htmlUrl;
+    private String avatar_url;
+    private String name;
+    private Integer public_repos;
 
+    // Constructor User()
+    public User(String login, String avatar_url, String name, Integer public_repos) {
+        this.login = login;
+        this.avatar_url = avatar_url;
+        this.name = name;
+        this.public_repos = public_repos;
+    }
+
+    // Getters e Setters
     public String getLogin() {
         return login;
     }
-    public String getAvatarUrl() {
-        return avatarUrl;
-    }
-    public String getHtmlUrl() {
-        return htmlUrl;
-    }
 
-    public void setAvatarUrl(String avatarUrl) {
-        this.avatarUrl = avatarUrl;
-    }
-    public void setHtmlUrl(String htmlUrl) {
-        this.htmlUrl = htmlUrl;
-    }
     public void setLogin(String login) {
         this.login = login;
     }
 
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    public void setAvatar_url(String avatar_url) {
+        this.avatar_url = avatar_url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPublic_repos() {
+        return public_repos;
+    }
+
+    public void setPublic_repos(Integer public_repos) {
+        this.public_repos = public_repos;
+    }
 }
