50c23fdc4a7dea17db5cf01a0c5d218168a5aa1b
Geraldo Júnior <geraldo.gj63@gmail.com>
2025-02-13 15:32:12 -0300

    Update RetrofitClient.java

--- app/src/main/java/com/example/github_viewer/api/RetrofitClient.java
+++ app/src/main/java/com/example/github_viewer/api/RetrofitClient.java
@@ -3,25 +3,30 @@ package com.example.github_viewer.api;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import java.io.IOException;
 
 public class RetrofitClient {
-    private static Retrofit retrofit = null;
+    private static Retrofit retrofit;
     private static final String BASE_URL = "https://api.github.com/";
-    private static final String GITHUB_TOKEN = "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"; // Substitua pelo seu token
 
     public static Retrofit getClient() {
         if (retrofit == null) {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);  // Mostra detalhes da requisição
+
             OkHttpClient client = new OkHttpClient.Builder()
                     .addInterceptor(new Interceptor() {
                         @Override
                         public okhttp3.Response intercept(Chain chain) throws IOException {
                             Request original = chain.request();
                             Request.Builder requestBuilder = original.newBuilder()
-                                    .header("Authorization", "token " + GITHUB_TOKEN)
+                                    // Remover a linha abaixo, pois não há mais necessidade de incluir o token
+                                    //.header("Authorization", "token " + GITHUB_TOKEN)
                                     .method(original.method(), original.body());
+
                             Request request = requestBuilder.build();
                             return chain.proceed(request);
                         }
