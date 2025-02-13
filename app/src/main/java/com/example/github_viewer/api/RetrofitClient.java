2b4e0e8323bcebb06ccea3daee4f59120e34c93c
Gerald0Juni0r <geraldo.gj63@gmail.com>
2025-02-13 17:12:00 -0300

    atualizando layout das telas

--- app/src/main/java/com/example/github_viewer/api/RetrofitClient.java
+++ app/src/main/java/com/example/github_viewer/api/RetrofitClient.java
@@ -1,7 +1,12 @@
 package com.example.github_viewer.api;
 
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import java.io.IOException;
 
 public class RetrofitClient {
     private static Retrofit retrofit;
@@ -9,8 +14,28 @@ public class RetrofitClient {
 
     public static Retrofit getClient() {
         if (retrofit == null) {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);  // Mostra detalhes da requisição
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public okhttp3.Response intercept(Chain chain) throws IOException {
+                            Request original = chain.request();
+                            Request.Builder requestBuilder = original.newBuilder()
+                                    // Remover a linha abaixo, pois não há mais necessidade de incluir o token
+                                    //.header("Authorization", "token " + GITHUB_TOKEN)
+                                    .method(original.method(), original.body());
+
+                            Request request = requestBuilder.build();
+                            return chain.proceed(request);
+                        }
+                    })
+                    .build();
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
